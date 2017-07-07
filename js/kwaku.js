9647187834cea6fd1530e3c1329681cb0cee7ccb
Kwaku60 <kwakuklausphoto@gmail.com>
2017-07-07 18:51:39 -0400

    updated joop

---
+++ js/kwaku.js
@@ -0,0 +1,17 @@
+$("#port").mouseenter (function(){
+
+
+$("#joop").attr("src","img/portfolio/joopfinalshot2.jpg");
+ 
+
+
+    });
+
+
+     $("#port").mouseleave (function(){
+
+
+$("#joop").attr("src","img/portfolio/joopfinalshot1.jpg"); 
+
+
+    });
